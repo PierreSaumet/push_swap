d7aae24bc50f5dfddf155c6cba3b0c5df26b1426
Pierre <psaumet.dvp@gmail.com>
2021-03-22 21:59:00 +0100

    adding, first fcxt to compare the string and get the command

--- headers/checker.h
+++ headers/checker.h
@@ -3,7 +3,7 @@
 
 
 
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
--- main.c
+++ main.c
@@ -141,13 +141,106 @@ void        ft_start(int argc, char **argv)
     
 }
 
+// ! Changer strcmp en ft_strcmp
+int     ft_check_cmd(char *line)
+{
+    if (strcmp(line, "sa") == 0)
+    {
+        // ft_sa
+        printf("cas SA\n");
+    }
+    else if (strcmp(line, "sb") == 0)
+    {
+        // ft_sb
+        printf("cas SB\n");
+    }
+    else if (strcmp(line, "ss") == 0)
+    {
+        // ft_ss
+        printf("cas SS\n");
+    }
+    else if (strcmp(line, "pa") == 0)
+    {
+        // ft_pa
+        printf("cas PA\n");
+    }
+    else if (strcmp(line, "pb") == 0)
+    {
+        // ft_pb
+        printf("cas PB\n");
+    }
+    else if (strcmp(line, "ra") == 0)
+    {
+        // ft_ra
+        printf("cas RA\n");
+    }
+    else if (strcmp(line, "rb") == 0)
+    {
+        // ft_rb
+        printf("cas RB\n");
+    }
+    else if (strcmp(line, "rr") == 0)
+    {
+        // ft_rr
+        printf("cas RR\n");
+    }
+    else if (strcmp(line, "rra") == 0)
+    {
+        // ft_rra
+        printf("cas RRA\n");
+    }
+    else if (strcmp(line, "rrb") == 0)
+    {
+        // ft_rrb
+        printf("cas RRB\n");
+    }
+    else if (strcmp(line, "rrr") == 0)
+    {
+        /// ft_rrr
+        printf("cas RRR\n");
+    }
+    else
+    {
+        printf("ERREUR CMD\n");
+        return (1);
+    }
+    free(line);
+    return (0);
+}
+
+int     ft_get_cmd(void)
+{
+    int ret_gnl;
+    char    *line;
+    int     ret;
+
+    ret = 0;
+    printf("\n\nDEBUT GNL\n");
+    ret_gnl = get_next_line(0, &line);
+    printf("premier gnl... ret_gnl = %d \t et line = -%s-\n", ret_gnl, line);
+    while (ret_gnl != 0)
+    {
+        printf("debut boucle\n");
+        ret = ft_check_cmd(line);
+        if (ret != 1)
+        {
+            //printf("line = -%s- et le retour de ft_check_cmd = %d\n", line, ret);
+            //free(line);
+            ret_gnl = get_next_line(0, &line);
+        }
+        else
+            break;
+        printf("fin de boucle\n");
+    }
+    printf("on sort\n");
+    free(line);
+    return (0);
+}
+
 int     main(int argc, char **argv)
 {
     int ret;
 
-    char *line;
-    int ret_gnl;
-    ret_gnl = 0;
     printf("argc = %d and argv[0] = %s and argv[1] = %s\n", argc, argv[0], argv[1]);
     ret = ft_check_args(argc, argv);
     if (ret == 1)
@@ -159,6 +252,12 @@ int     main(int argc, char **argv)
     {
         write(1, "OK\n", 3);
         ft_start(argc, argv);
+
+        if ((ft_get_cmd() == 1))
+            exit(EXIT_FAILURE);
+        //ft_get_cmd();
+        // get the commands
+        /*
         ret_gnl = get_next_line(0, &line);
         printf("FIRST LIGNE = %s\n", line);
         while (ret_gnl != 0)
@@ -167,7 +266,7 @@ int     main(int argc, char **argv)
             free(line);
             ret_gnl = get_next_line(0, &line);
         }
-        free(line);
+        free(line);*/
         //line= NULL;
     }
     //free(line);
